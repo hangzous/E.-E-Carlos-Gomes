efdc0a656bb70c3d92f6dbe5b35682d3722a63bf
Marcelo Cabral <69129820+hangzous@users.noreply.github.com>
2021-09-21 22:41:52 -0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,26 @@
-
 const hamburger = document.querySelector("#mobile_menu")
 const header_links = document.querySelector("header .default")
 const close_menu = document.querySelector("#close_menu")
 
-hamburger.addEventListener('click', () => {
-    header_links.classList.toggle('active')
-    document.body.style.overflow = 'hidden'
-})
 
-close_menu.addEventListener('click', () => {
+hamburger.addEventListener('click', open_menu) 
+
+function open_menu() {
     header_links.classList.toggle('active')
-    document.body.style.overflowY = 'auto'
-})
+    document.body.style.overflowY = 'hidden'
+}
+
+close_menu.addEventListener('click', x_menu)
+
+function x_menu() {
+    header_links.classList.toggle('active')
+    document.body.style.overflowY = 'scroll'
+}
+
+
+
+
+
+
+
+
---
+++ js/warning.js
@@ -0,0 +1,5 @@
+const post_button = document.querySelector("#add_post")
+
+post_button.addEventListener('click', () => {
+    alert('Serviço inoperante no momento.')    
+})
